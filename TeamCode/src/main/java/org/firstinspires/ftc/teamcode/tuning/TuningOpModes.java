dc3900ca571f42f0d8841ee3cfd5cd6d80fd4ace
Team14968-GL <386831@guhsd.net>
2025-12-04 16:34:48 -0800

    Added better support for FTC dashboard

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TuningOpModes.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TuningOpModes.java
@@ -191,7 +191,7 @@ public final class TuningOpModes {
                                 md.leftBack
                         ),
                         Arrays.asList(
-                                md.rightFront,
+                                md.rightBack,
                                 md.rightBack
                         ),
                         leftEncs,
