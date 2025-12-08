4282ed8e1b7a2e0ae010c813f79e26e0016e3146
Team14968-GL <386831@guhsd.net>
2025-12-08 13:44:56 -0800

    Updated ColorTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorTest.java
@@ -2,18 +2,29 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 
-@TeleOp(name = "Starter OpMode")
+@TeleOp(name = "Color Test")
 public class ColorTest extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        OpticalDistanceSensor Color = hardwareMap.get(OpticalDistanceSensor.class, "color");
+        NormalizedColorSensor Color = hardwareMap.get(NormalizedColorSensor.class, "color");
+        OpticalDistanceSensor Optical = hardwareMap.get(OpticalDistanceSensor.class, "color");
 
         waitForStart();
         if (opModeIsActive()) {
-
+            while (opModeIsActive()) {
+                telemetry.addData("OptLight", "%.3f", Optical.getLightDetected());
+                NormalizedRGBA colors = Color.getNormalizedColors();
+                telemetry.addData("NormAlpha", "%.3f", colors.alpha);
+                telemetry.addData("NormRed", "%.3f", colors.red);
+                telemetry.addData("NormGreen", "%.3f", colors.green);
+                telemetry.addData("NormBlue", "%.3f", colors.blue);
+                telemetry.update();
+            }
         }
     }
 }
