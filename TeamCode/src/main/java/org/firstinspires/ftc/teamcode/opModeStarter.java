da4c7a1962cf0a21f1ea41cc93c57db48172bb77
Team14968-GL <386831@guhsd.net>
2025-12-08 09:08:03 -0800

    Added Base OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModeStarter.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "Starter OpMode") // use @Autonomous for autonomous and use @TeleOp for teleop
+public class opModeStarter extends LinearOpMode {
+
+    /**
+     * This sample contains the bare minimum Blocks for any regular OpMode. The 3 blue Comment
+     * Blocks show where to place Initialization code (runs once, after touching the DS INIT
+     * button, and before touching the DS Start arrow), Run code (runs once, after touching
+     * Start), and Loop code (runs repeatedly while the OpMode is active, namely not Stopped).
+     */
+    @Override
+    public void runOpMode() {
+        DcMotor backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        DcMotor backRight = hardwareMap.get(DcMotor.class, "backRight");
+        DcMotor frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        DcMotor frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+
+        // Put initialization blocks here.
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        waitForStart();
+        if (opModeIsActive()) {
+            // Put run blocks here.
+
+        }
+    }
+}
