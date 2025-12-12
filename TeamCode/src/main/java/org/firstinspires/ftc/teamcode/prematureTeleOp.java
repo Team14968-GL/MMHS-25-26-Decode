14a4b935352352bad9dc81519752f9ed845dcf22
Team14968-GL <386831@guhsd.net>
2025-12-11 20:47:01 -0800

    Naming Consistency Issue Fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prematureTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prematureTeleOp.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
@@ -13,7 +12,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp(name = "BasicTeleopRJ2 (Blocks to Java)")
-public class PrematureTeleOp extends LinearOpMode {
+public class prematureTeleOp extends LinearOpMode {
 
     private DcMotor intakeMotor;
     private Servo goofyAhhhhFrontDoor;
