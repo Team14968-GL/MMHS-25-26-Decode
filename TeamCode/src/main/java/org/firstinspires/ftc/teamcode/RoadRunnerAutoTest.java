581a49642fa8e76c626c6fdbfa45bb4ac80defb0
Team14968-GL <386831@guhsd.net>
2026-01-06 23:06:24 -0800

    Began movement abstraction and expanded RRAT

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerAutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerAutoTest.java
@@ -8,9 +8,15 @@ import java.lang.Math;
 public class RoadRunnerAutoTest extends LinearOpMode {
         @Override
         public void runOpMode() {
-            MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(new Vector2d(1,1), Math.PI));
-            Action action = drive.actionBuilder(new Pose2d(new Vector2d(1,1), Math.PI/3))
-                    .splineTo(new Vector2d(1,1), Math.PI)
+            MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(new Vector2d(0,0), 0));
+            Action action = drive.actionBuilder(new Pose2d(new Vector2d(0,0), 0))
+                    .splineTo(new Vector2d(15,15), Math.toRadians(180))
+                    .lineToX(10)
+                    .lineToY(20)
+                    .splineTo(new Vector2d(15,15),0)
+                    .splineTo(new Vector2d(-15,-15), 0)
+                    .lineToY(0)
+                    .lineToX(0)
                     .build();
 
             waitForStart();
