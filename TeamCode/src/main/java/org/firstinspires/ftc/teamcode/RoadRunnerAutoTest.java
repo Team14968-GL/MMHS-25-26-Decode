0bcaa42d56d03ed8074e4b7849c338129eeefcc6
GL <386831@guhsd.net>
2026-01-07 14:03:10 -0800

    RRAT still fails

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerAutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerAutoTest.java
@@ -1,32 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.*;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import java.lang.Math;
 
+@Autonomous
 public class RoadRunnerAutoTest extends LinearOpMode {
     @Override
     public void runOpMode() {
         Pose2d beginPose = new Pose2d(0, 0, 0);
         MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
-        Action action = drive.actionBuilder(new Pose2d(new Vector2d(0, 0), 0))
-                .splineTo(new Vector2d(15, 15), Math.toRadians(180))
-                .lineToX(10)
-                .lineToY(20)
-                .splineTo(new Vector2d(15, 15), 0)
-                .splineTo(new Vector2d(-15, -15), 0)
-                .lineToY(0)
-                .lineToX(0)
-                .setTangent(Math.PI/4)
-                .build();
 
         waitForStart();
 
-
-        if (isStopRequested()) {
-            requestOpModeStop();
-        }
+        Actions.runBlocking(
+                drive.actionBuilder(beginPose)
+                        .splineTo(new Vector2d(15, 15), Math.toRadians(180))
+                        .lineToX(10)
+                        .lineToY(20)
+                        .splineTo(new Vector2d(15, 15), 0)
+                        .splineTo(new Vector2d(-15, -15), 0)
+                        .lineToY(0)
+                        .lineToX(0)
+                        .setTangent(Math.PI/4)
+                        .build());
     }
 }
